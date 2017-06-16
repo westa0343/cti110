8675dbca48884a5c68eb88c954684a3db958a166
westa0343 <westa0343@student.faytechcc.edu>
2017-06-16 15:02:06 -0400

    First Submission

---
+++ M2T1_SalesPrediction_AdrianWest.py
@@ -0,0 +1,9 @@
+#Calculating Sales Prediction
+# 6/16/2017
+#CTI-110 M2T1 - Sales Prediction
+#Adrian West
+#
+
+TotalSales = float(input("Enter projected total sales"))
+Profit = TotalSales * 0.23
+print(Profit)
